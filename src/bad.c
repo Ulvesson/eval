715c95eac4ab370123dd59ff497fb50a19fb76bd
Tommy Wohlfart <tommy.wohlfart@gmail.com>
2020-11-17 06:17:08 +0100

    Added more bad things.

--- src/bad.c
+++ src/bad.c
@@ -1,4 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <string.h>
+
+char* string_dup2(const char* str)
+{
+	size_t len = strlen(str);
+	char* dup = (char*)malloc(len + 1);
+	if (dup)
+		strcpy(dup, str);
+	return str;
+}
+
+// http://www.open-std.org/jtc1/sc22/wg14/www/docs/n1969.htm
+char* string_dup(const char* str)
+{
+	char* dup = (char*)malloc(strlen(str) + 1);
+	if (dup)
+		strcpy(dup, str);
+	return str;
+}
 
 char* copy(char* buf) {
 	char p[20];
@@ -9,6 +29,7 @@ char* copy(char* buf) {
 
 int main(int argc, char** argv)
 {
+	char* dup = string_dup2("What");
 	char* buf = malloc(44);
 	buf = copy(argv[0]);
 	printf("%s", buf);
