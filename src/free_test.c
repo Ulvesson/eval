1b41811b4cb2f4ce098f22e3f4269ddc54ff7f7d
Tommy Wohlfart <tommy.wohlfart@gmail.com>
2020-11-18 08:05:41 +0100

    Now free_test.c compiles.

--- src/free_test.c
+++ src/free_test.c
@@ -1,40 +1,59 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <malloc.h>
 
 #define MAX_STR	100
 
-struct data_t {
+typedef struct data_t {
 	char* str1;
 	char* str2;
 	char* str3;
-};
+} data;
 
-data_t* create_data() {
-	data_t* p = malloc(sizeof(data_t));
+data* create_data() {
+	data *p = malloc(sizeof(data));
 	if (p) {
-		p.str1 = malloc(MAX_STR);
-		p.str2 = malloc(MAX_STR);
-		p.str3 = malloc(MAX_STR);
-		strncpy(p.str1, "str1", 3);
-		strncpy(p.str2, "str2", 3);
-		strncpy(p.str3, "str3", 3);
-		p.str1[3] = '\0';
-		p.str2[3] = '\0';
-		p.str3[3] = '\0';
+		p->str1 = malloc(MAX_STR);
+		if (!p->str1) {
+			free(p);
+			return 0;
+		}
+		p->str2 = malloc(MAX_STR);
+		if (!p->str2) {
+			free(p->str1);
+			free(p);
+			return 0;
+		}
+		p->str3 = malloc(MAX_STR);
+		if (!p->str3) {
+			free(p->str1);
+			free(p->str2);
+			free(p);
+			return 0;
+		}
+		strncpy(p->str1, "str1", 3);
+		strncpy(p->str2, "str2", 3);
+		strncpy(p->str3, "str3", 3);
+		p->str1[3] = '\0';
+		p->str2[3] = '\0';
+		p->str3[3] = '\0';
 	}
 
 	return p;
 }
 
-void free_data(data_t *p) {
-	free(p.str1);
-	free(p.str2);
-	free(p.str3);
+void free_data(data *p) {
+	free(p->str1);
+	free(p->str2);
+	free(p->str3);
 	free(p);
 }
 
 int main() {
-	data_t* p = create_data();
-	printf("str1: %s str2: %s str3: %s", p.str1, p.str2, p.str3);
+	data* p = create_data();
+	printf("str1: %s str2: %s str3: %s", p->str1, p->str2, p->str3);
 	free_data(p);
 	return 0;
 }
